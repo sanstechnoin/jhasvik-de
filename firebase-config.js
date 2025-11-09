2d01a5e0406f9475726f6ae5cfea796e5a0a9998
sanstechnoin <65869568+sanstechnoin@users.noreply.github.com>
2025-11-09 02:38:31 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// This is the entire content of firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCy6yL6CMkKr3WiuZjpmRJckMxTCJ2uScw",
+  authDomain: "jhasvik-menu.firebaseapp.com",
+  projectId: "jhasvik-menu",
+  storageBucket: "jhasvik-menu.firebasestorage.app",
+  messagingSenderId: "290491779981",
+  appId: "1:290491779981:web:84b8c3f81a0b154f65cbc4"
+};
